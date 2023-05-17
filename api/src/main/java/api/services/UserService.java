328a011ed4cf1d66e85a02c12b6a5cafd7292b67
jvitor07 <jvitoroliveira0711@gmail.com>
2023-05-16 21:54:10 -0300

    feature(backend): Adiciona metodo que valida a unicidade do email do usuario.

--- api/src/main/java/api/services/UserService.java
+++ api/src/main/java/api/services/UserService.java
@@ -1,6 +1,7 @@
 package api.services;
 
 import api.contracts.ICrudService;
+import api.exceptions.BadRequestException;
 import api.models.User;
 import api.repositories.UserRepository;
 import lombok.Getter;
@@ -10,12 +11,23 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class UserService implements ICrudService<User, Long, UserRepository> {
+    public static final String EMAIL_NOT_UNIQUE_ERROR_MESSAGE = "O email informado já está sendo utilizado por outro usuário.";
     @Getter
     private final UserRepository repository;
 
     @Override
     public String getEntityMessageNotFound() {
         return "Não foi possível encontrar este usuário na nossa base de dados.";
+    }
+
+    public void validateEmailUniqueness(String email) {
+        if (Boolean.TRUE.equals(this.existsByEmail(email))) {
+           throw new BadRequestException("email", EMAIL_NOT_UNIQUE_ERROR_MESSAGE);
+        }
+    }
+
+    private Boolean existsByEmail(String email) {
+        return this.repository.existsByEmail(email);
     }
 
     @Override
